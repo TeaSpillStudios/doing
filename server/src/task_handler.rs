a3592bcd15080b5bf32ebdc8f3d97538a9f5d524
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-04-12 23:06:33 +0100

    Add Add a function to add a new section

--- server/src/task_handler.rs
+++ server/src/task_handler.rs
@@ -1,18 +1,25 @@
 use std::collections::HashMap;
 
 #[derive(Default)]
-pub struct TaskHandler {
-    sections: HashMap<String, Section>,
+pub struct TaskHandler<'a> {
+    sections: HashMap<&'a str, Section<'a>>,
     current_section: Option<String>,
 }
 
 #[derive(Default)]
-pub struct Section {
-    tasks: HashMap<String, Section>,
+pub struct Section<'a> {
+    tasks: HashMap<&'a str, Task>,
 }
 
 #[derive(Default)]
 pub struct Task {
     description: String,
     completed: bool,
+}
+
+impl<'a> TaskHandler<'a> {
+    pub fn add_section(&mut self, section_name: &'a str) {
+        self.sections.insert(section_name, Section::default());
+    }
+    // pub fn add_section(&mut self, task_name: &str, task_description: &str, completed_bool) {
 }
