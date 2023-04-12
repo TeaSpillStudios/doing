16c4963d5e9dcaccb47ae1a0acfd8cbe12dbbacd
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-04-12 23:03:14 +0100

    Define basic information for each part of the TaskManager

--- server/src/task_handler.rs
+++ server/src/task_handler.rs
@@ -1,6 +1,18 @@
+use std::collections::HashMap;
+
 #[derive(Default)]
-pub struct TaskHandler {}
+pub struct TaskHandler {
+    sections: HashMap<String, Section>,
+    current_section: Option<String>,
+}
+
 #[derive(Default)]
-pub struct Section {}
+pub struct Section {
+    tasks: HashMap<String, Section>,
+}
+
 #[derive(Default)]
-pub struct Task {}
+pub struct Task {
+    description: String,
+    completed: bool,
+}
