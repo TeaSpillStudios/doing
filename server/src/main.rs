f5d3619376f9e9e669cb859f3c1df7e73f32d31a
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-04-13 13:43:03 +0100

    Add basic recieving and debugging of commands/parameters

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,10 @@
 mod task_handler;
 use task_handler::*;
 
-use std::net::{TcpListener, TcpStream};
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     pretty_env_logger::init();
@@ -25,10 +28,37 @@ fn main() {
             }
         };
 
-        handle_request(stream);
+        handle_request(stream, &mut task_handler);
     }
 }
 
-fn handle_request(mut stream: TcpStream) {
+fn handle_request(mut stream: TcpStream, task_handler: &mut TaskHandler) {
     log::info!("Recieved a connection.");
+
+    let lines = BufReader::new(stream).lines();
+
+    let command = String::new();
+
+    let last_tine = match lines.last() {
+        Some(v) => v,
+        None => {
+            log::error!("No command passed.");
+            return;
+        }
+    };
+
+    let last_line = match &last_tine {
+        Ok(v) => v,
+        Err(e) => {
+            log::error!("{e}");
+            return;
+        }
+    };
+
+    let args: Vec<&str> = last_line.split("|").collect();
+    let command = args[0];
+    let args = args[1..].iter().map(|v| *v).collect::<Vec<&str>>();
+
+    dbg!(command);
+    dbg!(args);
 }
