d51c90f4cecf8ffcadafc4229bebc4d99951b44b
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-04-13 13:48:40 +0100

    Add a basic connection to the Client and send example data

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,10 @@
+use std::io::Write;
+use std::net::TcpStream;
+
+// TODO: Make an actual client.
+
 fn main() {
-    println!("Hello, world!");
+    let mut stream = TcpStream::connect("localhost:2500").unwrap();
+
+    stream.write(b"add_section|To do").unwrap();
 }
