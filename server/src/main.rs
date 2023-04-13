01061f8be3b7aad032050bcf11bfc8b961a9b376
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-04-13 13:08:27 +0100

    Ad a basic TcpListener

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,34 @@
 mod task_handler;
 use task_handler::*;
 
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
     pretty_env_logger::init();
 
-    let mut _task_handler = TaskHandler::default();
+    let mut task_handler = TaskHandler::default();
+
+    let listener = match TcpListener::bind("0.0.0.0:2500") {
+        Ok(v) => v,
+        Err(e) => {
+            log::error!("{e}");
+            return;
+        }
+    };
+
+    for stream in listener.incoming() {
+        let stream = match stream {
+            Ok(v) => v,
+            Err(e) => {
+                log::error!("{e}");
+                return;
+            }
+        };
+
+        handle_request(stream);
+    }
+}
+
+fn handle_request(mut stream: TcpStream) {
+    log::info!("Recieved a connection.");
 }
