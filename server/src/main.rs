3cbc8657fa51842e9b4e86440e04fe830ead6cc7
TeaSpillStudios <teaspillstudios@proton.me>
2023-04-30 00:48:04 +0100

    Setup a basic echo TCP server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,74 +1,46 @@
-use log::info;
-use std::{
-    io::{BufRead, BufReader},
-    net::{TcpListener, TcpStream},
-};
+// use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use crate::task_handler::Task;
+use tokio::net::TcpListener;
+
+use log::{error, info};
+
+// use log::info;
 
 mod task_handler;
-use task_handler::*;
+// use task_handler::{Task, TaskHandler};
 
-const PORT: &str = "2500";
+const ADDR: &str = "localhost:2500";
 
-fn main() {
+impl std::fmt::Display for Task {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.description)
+    }
+}
+
+#[tokio::main]
+async fn main() {
     pretty_env_logger::init();
 
-    let mut task_handler = TaskHandler::default();
+    let listener = TcpListener::bind(ADDR).await.unwrap();
 
-    info!("Listening on port {PORT}");
+    let server = async move {
+        while let Ok((mut stream, _socket_addr)) = listener.accept().await {
+            tokio::spawn(async move {
+                let (mut reader, mut writer) = stream.split();
 
-    let listener = match TcpListener::bind(&format!("0.0.0.0:{PORT}")) {
-        Ok(v) => v,
-        Err(e) => {
-            log::error!("{e}");
-            return;
+                match tokio::io::copy(&mut reader, &mut writer).await {
+                    Ok(amount) => {
+                        info!("Wrote {} bytes", amount);
+                    }
+                    Err(err) => {
+                        error!("IO error {:?}", err);
+                    }
+                }
+            });
         }
     };
 
-    for stream in listener.incoming() {
-        let stream = match stream {
-            Ok(v) => v,
-            Err(e) => {
-                log::error!("{e}");
-                return;
-            }
-        };
+    info!("Server running on {ADDR}");
 
-        handle_request(stream, &mut task_handler);
-    }
-}
-
-fn handle_request(mut stream: TcpStream, task_handler: &mut TaskHandler) {
-    log::info!("Recieved a connection.");
-
-    // TODO: Add error handling.
-    let lines = BufReader::new(stream).lines();
-
-    let last_tine = match lines.last() {
-        Some(v) => v,
-        None => {
-            log::error!("No command passed.");
-            return;
-        }
-    };
-
-    let last_line = match &last_tine {
-        Ok(v) => v,
-        Err(e) => {
-            log::error!("{e}");
-            return;
-        }
-    };
-
-    let args: Vec<&str> = last_line.split("|").collect();
-
-    handle_command_args(task_handler, args)
-}
-
-fn handle_command_args(task_handler: &mut TaskHandler, args: Vec<&str>) {
-    let command = args[0];
-
-    if args.len() > 2 {
-        println!("3 params detected");
-        println!("{}", args.len());
-    }
+    server.await;
 }
