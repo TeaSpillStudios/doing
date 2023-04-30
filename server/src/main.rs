042f63cdcfbfef89fdc8b0af61fc57e32904a910
TeaSpillStudios <teaspillstudios@proton.me>
2023-04-30 01:03:45 +0100

    Add some test tasks

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,5 @@
 // use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use crate::task_handler::Task;
+use crate::task_handler::{Task, TaskHandler};
 use tokio::net::TcpListener;
 
 use log::{error, info};
@@ -17,9 +17,31 @@ impl std::fmt::Display for Task {
     }
 }
 
+// Add some test tasks.
+fn setup_test_task_handler(task_handler: &mut TaskHandler) {
+    task_handler.select_section("Test");
+    task_handler.add_task("Test-task1", "Hi", false);
+
+    task_handler.add_task("Test-task2", "Hi", false);
+    task_handler.add_task("Test-task3", "Hi", false);
+    task_handler.add_task("Test-task4", "Hi", false);
+
+    task_handler.add_section("Testing");
+    task_handler.select_section("Testing");
+
+    task_handler.add_task("Test-task5", "Hi", false);
+    task_handler.add_task("Test-task6", "Hi", false);
+    task_handler.add_task("Test-task7", "Hi", false);
+    task_handler.add_task("Test-task8", "Hi", false);
+}
+
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
+
+    let mut task_handler = TaskHandler::default();
+
+    setup_test_task_handler(&mut task_handler);
 
     let listener = TcpListener::bind(ADDR).await.unwrap();
 
