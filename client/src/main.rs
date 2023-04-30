046b67ec2186cf189130d53f29e365d773d634cf
TeaSpillStudios <teaspillstudios@proton.me>
2023-04-30 01:00:43 +0100

    Add a test client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,10 +1,35 @@
-use std::io::Write;
-use std::net::TcpStream;
+use log::{error, info};
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
 
 // TODO: Make an actual client.
 
 fn main() {
-    let mut stream = TcpStream::connect("localhost:2500").unwrap();
+    pretty_env_logger::init();
 
-    stream.write(b"add_section|To do").unwrap();
+    let mut stream = match TcpStream::connect("localhost:2500") {
+        Ok(v) => v,
+        Err(e) => {
+            error!("Connection refused.\n                 ╰ {e}");
+            return;
+        }
+    };
+
+    info!("Writing");
+
+    // stream.write(b"list|To do|Hi\n").unwrap();
+    stream.write(b"list|test").unwrap();
+
+    info!("Reading\n");
+
+    let mut buffer = [0; 8096];
+    let bytes_read = stream.read(&mut buffer).unwrap();
+    let response = String::from_utf8_lossy(&buffer[..bytes_read])
+        .lines()
+        .map(|v| format!("    {v}\n"))
+        .collect::<String>();
+
+    info!("Response: \n{}", response);
 }
