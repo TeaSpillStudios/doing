95efc3f2d6fdaf66ea7983fdff6c2e893ea07755
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-05-07 20:54:41 +0100

    Write soem tests for getting information from the server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,35 +1,43 @@
-use log::{error, info};
 use std::{
     io::{Read, Write},
-    net::TcpStream,
+    net::{Shutdown, TcpStream},
+    thread,
+    time::Duration,
 };
+use tracing::{error, info};
 
-// TODO: Make an actual client.
-
-fn main() {
-    pretty_env_logger::init();
-
+fn execute_command(command: &str) -> String {
     let mut stream = match TcpStream::connect("localhost:2500") {
         Ok(v) => v,
         Err(e) => {
             error!("Connection refused.\n                 ╰ {e}");
-            return;
+            return String::from("Failure.");
         }
     };
 
-    info!("Writing");
-
-    // stream.write(b"list|To do|Hi\n").unwrap();
-    stream.write(b"list|test").unwrap();
-
-    info!("Reading\n");
+    stream.write(command.as_bytes()).unwrap();
 
     let mut buffer = [0; 8096];
     let bytes_read = stream.read(&mut buffer).unwrap();
-    let response = String::from_utf8_lossy(&buffer[..bytes_read])
+
+    if let Err(e) = stream.shutdown(Shutdown::Both) {
+        error!("Failed to shutdown stream: {e}");
+    }
+
+    String::from_utf8_lossy(&buffer[..bytes_read])
         .lines()
         .map(|v| format!("    {v}\n"))
-        .collect::<String>();
+        .collect::<String>()
+}
 
+fn main() {
+    tracing_subscriber::fmt().without_time().init();
+
+    let response = execute_command("list|test");
+    info!("Response: \n{}", response);
+
+    thread::sleep(Duration::from_millis(100));
+
+    let response = execute_command("list|test");
     info!("Response: \n{}", response);
 }
