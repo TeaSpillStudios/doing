269605d5847537b11cfe81b16bfba2ebbf7cf357
TeaSpillStudios <TeaSpillStudios@proton.me>
2023-05-07 14:01:46 +0100

    Add basic handling of stream data

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,15 @@
+use std::sync::Arc;
+use tokio::sync::{Mutex, MutexGuard};
+
 use server::tasks::TaskHandler;
-use tokio::net::TcpListener;
+use tokio::{io::AsyncWriteExt, net::TcpListener};
 
 use log::{error, info};
 
 const ADDR: &str = "localhost:2500";
 
 // Add some test tasks.
-fn setup_test_task_handler(task_handler: &mut TaskHandler) {
+fn setup_test_task_handler(task_handler: &mut MutexGuard<'_, TaskHandler<'_>>) {
     task_handler.select_section("Test");
     task_handler.add_task("Test-task1", "Hi", false);
 
@@ -27,23 +30,29 @@ fn setup_test_task_handler(task_handler: &mut TaskHandler) {
 async fn main() {
     pretty_env_logger::init();
 
-    let mut task_handler = TaskHandler::default();
+    let task_handler = Arc::new(Mutex::new(TaskHandler::default())).clone();
 
-    setup_test_task_handler(&mut task_handler);
+    setup_test_task_handler(&mut task_handler.lock().await);
 
     let listener = TcpListener::bind(ADDR).await.unwrap();
 
     let server = async move {
         while let Ok((mut stream, _socket_addr)) = listener.accept().await {
+            let task_handler = task_handler.clone();
             tokio::spawn(async move {
-                let (mut reader, mut writer) = stream.split();
+                let map = task_handler.lock().await;
 
-                match tokio::io::copy(&mut reader, &mut writer).await {
-                    Ok(amount) => {
-                        info!("Wrote {} bytes", amount);
-                    }
-                    Err(err) => {
-                        error!("IO error {:?}", err);
+                println!("Current tasks:");
+
+                for task in map.get_tasks().unwrap() {
+                    println!("    {} - {}", task.0, task.1);
+                }
+
+                match stream.write(b"Testing").await {
+                    Ok(_) => {}
+                    Err(e) => {
+                        error!("Error writing to stream: {e}");
+                        return;
                     }
                 }
             });
